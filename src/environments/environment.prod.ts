8da2ff857a995d0b42820c8c6412aade12e95d27
Luferat <contato@luferat.net>
2023-09-20 16:55:18 -0300

    Integração com Firebase.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB3qGcFb11tOQGm2_vOnUxd7rQaBhcMkPQ",
+    authDomain: "ionteste-2023.firebaseapp.com",
+    projectId: "ionteste-2023",
+    storageBucket: "ionteste-2023.appspot.com",
+    messagingSenderId: "788295881622",
+    appId: "1:788295881622:web:b1ea5f214104177569c3b7"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB3qGcFb11tOQGm2_vOnUxd7rQaBhcMkPQ",
+    authDomain: "ionteste-2023.firebaseapp.com",
+    projectId: "ionteste-2023",
+    storageBucket: "ionteste-2023.appspot.com",
+    messagingSenderId: "788295881622",
+    appId: "1:788295881622:web:b1ea5f214104177569c3b7"
+  }
 };
 
 /*
