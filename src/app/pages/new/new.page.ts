084860d61488156276022d29e56c9fa7ff0fc582
xpoquerx <caio_pinhop@hotmail.com>
2023-10-11 16:31:26 -0300

    final de aula

--- src/app/pages/new/new.page.ts
+++ src/app/pages/new/new.page.ts
@@ -3,8 +3,8 @@ import { Router } from '@angular/router';
 
 import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import { getDownloadURL, getStorage, ref, uploadString } from 'firebase/storage';
 import { environment } from 'src/environments/environment';
 
 import { Geolocation } from '@capacitor/geolocation';
@@ -53,7 +53,16 @@ export class NewPage implements OnInit {
   geolocation = '';
 
   // Services de uso geral.
-  tools!: ToolsService;
+  tools = inject(ToolsService);
+
+  // Formato da imagem.
+  photoFormat = '';
+
+  // Define a coleção onde os contatos são armazenados.
+  dbCollection = collection(this.db, environment.dbCollection);
+
+  // Mostra formulário ainda não enviado.
+  sended = false;
 
   constructor() { }
 
@@ -70,7 +79,7 @@ export class NewPage implements OnInit {
     // Obtém geolocalização.
     Geolocation.getCurrentPosition()
       .then((x) => {
-        this.geolocation = `${x.coords.latitude}↕, ${x.coords.longitude}↔`;
+        this.geolocation = `${x.coords.latitude}, ${x.coords.longitude}`;
         this.newDocument.geolocation = this.geolocation;
       });
 
@@ -93,6 +102,9 @@ export class NewPage implements OnInit {
 
         // Envia para a view.
         this.newDocument.image = x.dataUrl + '';
+
+        // Atualiza formato da imagem.
+        this.photoFormat = x.format;
       })
 
     } else {
@@ -106,16 +118,93 @@ export class NewPage implements OnInit {
   change() {
     if (
       this.tools.stripTags(this.newDocument.name).length > 2 &&
-      this.tools.stripTags(this.newDocument.description).length > 5 &&
+      this.tools.stripTags(this.newDocument.description).length > 4 &&
       this.tools.stripTags(this.newDocument.location).length > 3
     ) this.btnDisabled = false;
     else this.btnDisabled = true;
   }
 
-  // Monitora situação do toggle do GPS.
-  toggleChange() { }
+  // Mostra/Oculta geolocalização no documento.
+  toggleChange() {
+    this.newDocument.geolocation = (this.toggleGPS) ? this.geolocation : '';
+  }
 
   // Salva dados do documento no back-end.
-  saveDocument() { }
+  saveDocument() {
+
+    // Valida campos.
+    if (this.btnDisabled) return false;
+
+    // Obtém a data atual do documento.
+    this.newDocument.date = this.tools.now();
+
+    // Se atualizou a foto, ou seja, não está usando a foto padrão.
+    if (this.newDocument.image !== '/assets/generic.png') {
+
+      // Cria um nome aleatório para a foto usando 'getRandomChars()' e adiciona o formato.
+      let storageRef = ref(this.storage, `${this.tools.getRandomChars(10)}.${this.photoFormat}`);
+
+      /**
+       * Envia a foto para o servidor no formato 'String/Base64'.
+       * Referências: https://firebase.google.com/docs/storage/web/upload-files?hl=pt-br
+       **/
+      uploadString(
+        storageRef,                                      // Dome da imagem.
+        this.newDocument.image.split(',')[1],         // Dados binários da imagem.
+        'base64',                                        // Formato dos dados.
+        { contentType: `image/${this.photoFormat}` }     // Formato da foto.
+      ).then(() => {
+
+        /**
+         * Se salvou a imagem, obtém a URL desta.
+         * Referências: https://firebase.google.com/docs/storage/web/download-files?hl=pt-br#download_data_via_url
+         **/
+        getDownloadURL(ref(storageRef))
+          .then((response) => {
+
+            // Salva a URL no campo 'image' do formulário.
+            this.newDocument.image = response
+
+            // Envia o formulário para o Firestore.
+            this.saveForm();
+          })
+      });
+
+      // Se não alterou a foto.
+    } else {
+
+      // Mantém a foto padrão e envia o formulário para o Firestore.
+      this.saveForm();
+    }
+
+    // Conclui 'sendForm()'.
+    return true;
+  }
+
+  // Salva metadados no Firestore.
+  saveForm() {
+    
+     // Salva formulário no banco de dados.
+     addDoc(this.dbCollection, this.newDocument)
+
+     // Se teve sucesso, oculta formulário e agradece ao usuário.
+     .then((data) => {
+       // console.log('Contato salvo com o Id: ' + data.id);
+       this.sended = true;
+     })
+
+     // Se falhou, exibe mensagem de erro no console.
+     .catch((error) => {
+       console.error(error);
+     })
+
+   }
+
+   // Reinicia o formulário.
+   reload(){
+
+    // Recarrega a página.
+    location.href = '/new'
+   }
 
 }
